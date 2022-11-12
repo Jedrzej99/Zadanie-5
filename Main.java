2d147cdb5202432075bfb7edb6b21b8cd3642b12
Jedrzej99 <118077779+Jedrzej99@users.noreply.github.com>
2022-11-13 00:43:13 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Car nissan = new Car("Juke", "Nissan", 2022, 60000.0);
+
+        Human Damian = new Human();
+        Damian.Name="Damian";
+        Damian.Surname="Kowalski";
+        Damian.setSalary(3500.0);
+        Damian.getSalary();
+        Damian.setCar(nissan);
+
+
+        }
+    }
